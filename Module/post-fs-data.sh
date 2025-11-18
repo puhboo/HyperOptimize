5f64c162ee9b6f11fd1858df79bdcb17017b52f6
puhbu <239215561+puhboo@users.noreply.github.com>
2025-11-18 19:19:48 +0200

    Add Vulkan detector for MTK

--- Module/post-fs-data.sh
+++ Module/post-fs-data.sh
@@ -32,11 +32,23 @@ else
     resetprop vendor.mi.mpbe.ioturbo.enable 1
 fi
 
-# Vulkan Enabler
+# Vulkan Enabler for QCOM
 if [ -f "/system/vendor/etc/permissions/android.hardware.vulkan.version-1_3.xml" ] && [[ $(getprop ro.build.version.sdk) -ge 33 ]]; then
     resetprop debug.hwui.renderer skiavk
     resetprop ro.hwui.use_vulkan true
     resetprop ro.hardware.vulkan adreno
+    # resetprop debug.renderengine.graphite true
+    resetprop debug.renderengine.vulkan true
+    resetprop debug.renderengine.backend skiavkthreaded
+else
+    resetprop debug.renderengine.backend skiaglthreaded
+fi
+
+# Vulkan Enabler for MTK, meow is actually the name of their renderer
+if [ -f "/system/vendor/etc/permissions/android.hardware.vulkan.version-1_3.prebuilt.xml" ] && [[ $(getprop ro.build.version.sdk) -ge 33 ]]; then
+    resetprop debug.hwui.renderer skiavk
+    resetprop ro.hwui.use_vulkan true
+    resetprop ro.hardware.vulkan meow
     # resetprop debug.renderengine.graphite true
     resetprop debug.renderengine.vulkan true
     resetprop debug.renderengine.backend skiavkthreaded
